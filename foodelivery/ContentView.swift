5fc5efac02b461c8579239aceec081bbd661a7de
Saravanan v <v.saravanan.c@gmail.com>
2026-05-29 20:48:55 +0530

    Initial Commit

---
+++ foodelivery/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  foodelivery
+//
+//  Created by Saravanan V on 29/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ foodelivery/foodeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  foodeliveryApp.swift
+//  foodelivery
+//
+//  Created by Saravanan V on 29/05/26.
+//
+
+import SwiftUI
+
+@main
+struct foodeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
